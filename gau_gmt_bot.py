c3a238cabd53e282a36198972a7e28ca89123e94
a-stands4-A <akim.idrisov@mail.ru>
2023-04-27 23:04:57 +0300

    reply by uppered text from user and text with 2 or more words

--- gau_gmt_bot.py
+++ gau_gmt_bot.py
@@ -8,5 +8,17 @@ dp = Dispatcher(bot)
 async def echo(msg: types.Message):
     await msg.answer(text=msg.text + ", GIT GUT, MATE", reply_markup=types .ReplyKeyboardRemove())
 
+
+# # practice 1.1
+# @dp.message_handler()
+# async def echo_upper(message: types.Message):
+#     await message.answer(text=message.text.upper())
+
+# practice 1.2
+@dp.message_handler()
+async def mes_count(message: types.Message):
+    if message.text.count(' ') >= 1:
+        await message.answer(text=message.text)
+
 if __name__ == '__main__':
    executor.start_polling(dp)
