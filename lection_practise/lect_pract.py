df1dec6e422f84b8c94f046c1fbf0e9a679f1888
a-stands4-A <akim.idrisov@mail.ru>
2023-05-02 21:37:40 +0300

    урок 5-6: работа со стикерами

--- lection_practise/lect_pract.py
+++ lection_practise/lect_pract.py
@@ -1,35 +1,64 @@
 import string
 from aiogram import Bot, Dispatcher, executor, types
 import random as rdm
-
-from  config import TOKEN
+import pyperclip  # для записи текста в clipboard
+from config import TOKEN
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 
-count = 1
+# # lect
+# @dp.message_handler(commands=["s"])
+# async def start_CMD(message: types.Message):
+#     # text = "*HI!* lets _kick_ *zayavki*"
+#     await message.answer(text=text, parse_mode="MARKDOWN")
+#     # pyperclip.copy(text)
+#
+#
+# @dp.message_handler(commands=["g"])
+# async def sticker_cmd(message: types.Message):
+#     await bot.send_sticker(message.from_user.id, sticker="CAACAgIAAxkBAAEI1AZkUU9Qa2Q3mpLEbcAn0681FKJkbQACpRAAArRFoEpqI1qAWc6jRy8E")
+#     await message.delete()
+#
+# @dp.message_handler()
+# async def emooo(message: types.Message):
+#     await message.reply(message.text + "🛹")
 
-@dp.message_handler(commands=["desctiption"])
-async def desc_c(message: types.Message):
-    await message.answer("бот что-то умеет")
-    await message.delete()
+###pract
+HELP_CMD = """
+*/help* -_показывает список команд_
+*/g* - _мимишный кот_
+"""
 
-@dp.message_handler(commands=["count"])
-async def count_c(message: types.Message):
-    global count
-    await message.answer(f"{count=}")
-    count += 1
+async def on_start_up(_):
+    print("Я запустился")
+
+@dp.message_handler(content_types=["sticker"])
+async def sticker_id(mess: types.Message):
+    await mess.answer(mess.sticker.file_id)
+
+
+@dp.message_handler(commands=['h'])
+async def helpCmd(mess: types.Message):
+    await mess.reply(text=HELP_CMD, parse_mode="MARKDOWN")
+
+@dp.message_handler(commands=["g"])
+async def s_st(message: types.Message):
+    await message.answer("Смотри на котяру:")
+    await bot.send_sticker(message.from_user.id,
+                           sticker="CAACAgUAAxkBAAEI1ApkUVE6B_OoQ5R43oa2ijrdGFFoaQAC1gEAAt8fchn5W4LVs8ZxXy8E")
+
 
 @dp.message_handler()
-async def check_zero(message: types.Message):
-    if '0' in message.text:
-        return await message.answer(text="YES")
-    await message.answer(text="NO")
+async def heart(message: types.Message):
+    if message.text == "❤️":
+        await message.answer(text="❤️‍🔥")
+
 
 @dp.message_handler()
-async def rdm_ASCII(message: types.Message):
-    await message.reply(rdm.choice(string.ascii_letters))
+async def galy(message: types.Message):
+    await message.answer(text=str(message.text.count("✅")))
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    executor.start_polling(dp, on_startup=on_start_up)
