3308878a736d1a99a296782381105b97c1164aea
a-stands4-A <akim.idrisov@mail.ru>
2023-05-02 20:03:25 +0300

    практика к 4му уроку: подсчет вызовов, команда описания, проверка на налчиие '0' в сообщии пользователя

---
+++ lection_practise/lect_pract.py
@@ -0,0 +1,36 @@
+import string
+
+from aiogram import Bot, Dispatcher, executor, types
+import random as rdm
+
+from  config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+count = 1
+
+@dp.message_handler(commands=["desctiption"])
+async def desc_c(message: types.Message):
+    await message.answer("бот что-то умеет")
+    await message.delete()
+
+@dp.message_handler(commands=["count"])
+async def count_c(message: types.Message):
+    global count
+    await message.answer(f"{count=}")
+    count += 1
+
+@dp.message_handler()
+async def check_zero(message: types.Message):
+    if '0' in message.text:
+        return await message.answer(text="YES")
+    await message.answer(text="NO")
+
+@dp.message_handler()
+async def rdm_ASCII(message: types.Message):
+    await message.reply(rdm.choice(string.ascii_letters))
+
+
+if "__name__" == "__main__":
+    executor.start_polling(dp)
