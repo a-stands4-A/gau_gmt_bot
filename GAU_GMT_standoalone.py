aa703649ca446ebd20fee8da8d4f14d7c43300dd
a-stands4-A <akim.idrisov@mail.ru>
2023-08-07 00:06:41 +0300

    gui changes;

--- GAU_GMT_standoalone.py
+++ GAU_GMT_standoalone.py
@@ -4,6 +4,7 @@ import pymorphy2
 from tkinter import messagebox
 from tkinter import scrolledtext
 
+
 def paste_from_clipboard():
     text = pyperclip.paste()
     input_field.delete(0, tk.END)
@@ -27,9 +28,11 @@ def copy_1_to_clipboard():
 
 def roditPadesh():
     result = []
-    text = input_field.get()
+    text = input_field.get().replace(".", ". ")
+    text = text.replace("\n", "")
+    text = text.split('. ')
     # Разбиваем текст на фразы по символам переноса строки
-    phrases = text.split('\n')
+    phrases = "\n".join(text).split('\n')
     for index, phrase in enumerate(phrases):
         dummyString = []
         for word in phrase.split():
@@ -48,14 +51,6 @@ def inflect_to_genitive(word):
     genitive_word = parsed_word.inflect({'gent'})
     return genitive_word.word if genitive_word else word
 
-def zakl():
-    text = input_field_1.get().replace("\n", "")
-    text = text.replace(".", ". ")
-    text = text.split('. ')
-    text = "\n".join(text)
-    # pyperclip.copy(text)
-    # input_field.delete(0, tk.END)
-    return text
 
 def cmdT9():
     text = input_field.get()
@@ -83,6 +78,13 @@ def update_suggested_phrase(event):
     else:
         input_field_1.delete(0, tk.END)
 
+def zakl():
+    text = input_field_1.get().replace(".", ". ")
+    text = text.replace("\n", "")
+    text = text.split('. ')
+    text = "\n".join(text)
+    return text
+
 def complexZakl():
     someData0 = roditPadesh().split("\n")
     someData1 = zakl().split("\n")
@@ -99,26 +101,40 @@ def complexZakl():
 def writeReadDict2Txt(choice):
     import pprint
     if choice == 1:
+        with open('data_0.txt', 'r') as file:
+            haha_0 = eval(file.read())
         with open('data.txt', 'r') as file:
             haha = eval(file.read())
-        # haha = sorted(haha.values())
-        haha1 = dict(sorted(haha.items(), key=lambda item: item[1]))
+        temp = haha_0 | haha  #Python 3.9+: The merge operator | now works for dictionaries:
+        haha_1 = dict(sorted(haha.items(), key=lambda item: item[1]))
+        with open('data_0.txt', 'w') as file:
+            pprint.pprint(haha_0, stream=file)
         # Запись словаря в текстовый файл в человекочитаемой форме
         with open('data.txt', 'w') as file:
-            pprint.pprint(haha1, stream=file)
-        return haha1
+            pprint.pprint(haha_1, stream=file)
+        return temp
     elif choice == 0:
         # Прочитать словарь из текстового файла
+        with open('data_0.txt', 'r') as file:
+            haha_0 = eval(file.read())
         with open('data.txt', 'r') as file:
             haha = eval(file.read())
-        haha1 = dict(sorted(haha.items(), key=lambda item: item[1]))
+        haha1 = haha_0 | haha  #Python 3.9+: The merge operator | now works for dictionaries:
         return haha1
 
+#Define a function to clear the content of the text widget
+def click_input_field(event):
+   input_field.delete(0, "end")
+   input_field.unbind('<Button-1>', clicked_input_field)
+
+def click_input_field_1(event):
+   input_field_1.delete(0, "end")
+   input_field_1.unbind('<Button-1>', clicked_input_field_1)
 haha = writeReadDict2Txt(0)
 
 HELP_CMD = ""
 for i in haha.keys():
-    HELP_CMD += f"{haha[i]} - {i}\n"
+    HELP_CMD += f"{i} => {haha[i]}\n"
 
 is_info = False
 
@@ -128,37 +144,36 @@ root.attributes("-topmost", True)
 root.geometry("200x315")
 
 # Create input field label and entry
-input_label = tk.Label(root, text="Single (компонент):")
-input_label.pack()
+input_label = tk.Label(root, text="T9/Single (компонент)", height=1)
+input_label.pack(fill=tk.X)
 input_field = tk.Entry(root)
-input_field.pack()
+input_field.insert(0, "Ключ или неисправность")
+#Bind the Entry widget with Mouse Button to clear the content
+clicked_input_field = input_field.bind('<Button-1>', click_input_field)
+input_field.pack(padx=10, pady=5, fill=tk.X)
 
 # Create output field label and entry
-input_label_1 = tk.Label(root, text="Complex (неисправность):")
-input_label_1.pack()
+input_label_1 = tk.Label(root, text="Complex (неисправность)")
+input_label_1.pack(fill=tk.X)
 input_field_1 = tk.Entry(root)
-input_field_1.pack()
+input_field_1.insert(0, "Неисправности")
+#Bind the Entry widget with Mouse Button to clear the content
+clicked_input_field_1 = input_field_1.bind('<Button-1>', click_input_field_1)
+input_field_1.pack(padx=10, pady=5, fill=tk.X)
 
 # Create command buttons
-helpCMD_button = tk.Button(root, text="Т9", command=cmdT9)
-helpCMD_button.pack()
-# component_button = tk.Button(root, text="Родительный падеж", command=roditPadesh)
-# component_button.pack()
+T9_button = tk.Button(root, text="T9", command=cmdT9)
+T9_button.pack(padx=10, pady=5, fill=tk.X)
 zakl_button = tk.Button(root, text="Итоговое заключение", command=complexZakl)
-zakl_button.pack()
+zakl_button.pack(padx=10, pady=5, fill=tk.X)
 # noinspection PyTypeChecker
-show_info_message = tk.Button(root, text="show_info_message", command=show_info_message)
-show_info_message.pack()
+show_info_message = tk.Button(root, text="Info message", command=show_info_message)
+show_info_message.pack(padx=10, pady=5, fill=tk.X)
 
-paste_button = tk.Button(root, text="Вставить из буфера в Single", command=paste_from_clipboard)
-paste_button.pack()
-# copy_button = tk.Button(root, text="Скопировать в буфер Single", command=copy_to_clipboard)
-# copy_button.pack()
-paste_button = tk.Button(root, text="Вставить из буфера в Complex", command=paste_1_from_clipboard)
-paste_button.pack()
-# copy_button = tk.Button(root, text="Скопировать в буфер Complex", command=copy_1_to_clipboard)
-# copy_button.pack()
-
+paste_button = tk.Button(root, text="Из буфера в Single", command=paste_from_clipboard)
+paste_button.pack(padx=10, pady=5, fill=tk.X)
+paste_button = tk.Button(root, text="Из буфера в Complex", command=paste_1_from_clipboard)
+paste_button.pack(padx=10, pady=5, fill=tk.X)
 
 # Bind the update_suggested_phrase function to the "<KeyRelease>" event on input_field
 input_field.bind("<KeyRelease>", update_suggested_phrase)
