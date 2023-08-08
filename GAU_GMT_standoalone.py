075b7035e8fd3fdf1d326fc3fa15fdd522235109
a-stands4-A <akim.idrisov@mail.ru>
2023-08-08 19:47:44 +0300

    улучшена генерация итогового заключения

--- GAU_GMT_standoalone.py
+++ GAU_GMT_standoalone.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import pyperclip
 import pymorphy2
+from pymorphy2.shapes import restore_capitalization
+
 from tkinter import messagebox
 from tkinter import scrolledtext
 
@@ -10,46 +12,51 @@ def paste_from_clipboard():
     input_field.delete(0, tk.END)
     input_field.insert(tk.END, text)
 
+
 def paste_1_from_clipboard():
     text = pyperclip.paste()
     input_field_1.delete(0, tk.END)
     input_field_1.insert(tk.END, text)
+
 
 def copy_to_clipboard():
     text = input_field.get()
     root.clipboard_clear()
     pyperclip.copy(text)
     # root.clipboard_append(text)
+
+
 def copy_1_to_clipboard():
     text = input_field_1.get()
     root.clipboard_clear()
     pyperclip.copy(text)
     # root.clipboard_append(text)
 
+
 def roditPadesh():
+    """
+    sourse:https://groups.google.com/g/pymorphy/c/zJ0E6pJAypU
+    :return:
+    """
+    morph = pymorphy2.MorphAnalyzer()
     result = []
-    text = input_field.get().replace(".", ". ")
-    text = text.replace("\n", "")
-    text = text.split('. ')
+    text = input_field.get()
     # Разбиваем текст на фразы по символам переноса строки
-    phrases = "\n".join(text).split('\n')
-    for index, phrase in enumerate(phrases):
-        dummyString = []
-        for word in phrase.split():
-            genitive_word = inflect_to_genitive(word)
-            dummyString.append(genitive_word)
-        genitive_phrase = " ".join(dummyString)
-        genitive_phrase += '.'
-        result.append(genitive_phrase)
-    itog = "\n".join(result)
-    # pyperclip.copy(itog)
+    phrases = text.split('\n')
+    itog = ".\n".join(inflect_all(morph, phrase, {'gent'}) for  phrase in phrases)
     return itog
 
-def inflect_to_genitive(word):
-    morph = pymorphy2.MorphAnalyzer()
-    parsed_word = morph.parse(word)[0]
-    genitive_word = parsed_word.inflect({'gent'})
-    return genitive_word.word if genitive_word else word
+
+def inflect_all(morph, text, required_grammemes):
+    tokens = text.split()
+    inflected = [morph.parse(tok)[0].inflect(required_grammemes).word for tok in tokens]
+    return " ".join(inflected)
+
+
+def zakl():
+    text = input_field_1.get().replace("\n", "").split(".")
+    text = "\n".join(text)
+    return text
 
 
 def cmdT9():
@@ -58,6 +65,7 @@ def cmdT9():
         pyperclip.copy(haha[text.lower()])
     input_field.delete(0, tk.END)
     input_field_1.delete(0, tk.END)
+
 
 def show_info_message():
     if not hasattr(root, 'info_window'):
@@ -69,6 +77,8 @@ def show_info_message():
     else:
         root.info_window.destroy()
         del root.info_window
+
+
 def update_suggested_phrase(event):
     text = input_field.get().lower()
     if text in haha:
@@ -78,25 +88,21 @@ def update_suggested_phrase(event):
     else:
         input_field_1.delete(0, tk.END)
 
-def zakl():
-    text = input_field_1.get().replace(".", ". ")
-    text = text.replace("\n", "")
-    text = text.split('. ')
-    text = "\n".join(text)
-    return text
 
 def complexZakl():
-    someData0 = roditPadesh().split("\n")
-    someData1 = zakl().split("\n")
+    someData0 = roditPadesh().replace("\t", "").replace("\r", "").split("\n")[0:-1]
+    someData1 = zakl().replace("\t", "").replace("\r", "").split("\n")[0:-1]
+    zipped_values = zip(someData0, someData1)
+    zipped_list = list(zipped_values)
     someData = []
-    for ind, elem in enumerate(someData0):
-        temp = someData1[ind].replace("\r", "") + " " + someData0[ind]
+    for elem in zipped_list:
+        temp = elem[1] + " " + elem[0]
         someData.append(temp)
-    someData[-1], someData[0] = someData[0], someData[-1]
     combined_string = " ".join(someData)
     pyperclip.copy(combined_string)
     input_field.delete(0, tk.END)
     input_field_1.delete(0, tk.END)
+
 
 def writeReadDict2Txt(choice):
     import pprint
@@ -105,7 +111,7 @@ def writeReadDict2Txt(choice):
             haha_0 = eval(file.read())
         with open('data.txt', 'r') as file:
             haha = eval(file.read())
-        temp = haha_0 | haha  #Python 3.9+: The merge operator | now works for dictionaries:
+        temp = haha_0 | haha  # Python 3.9+: The merge operator | now works for dictionaries:
         haha_1 = dict(sorted(haha.items(), key=lambda item: item[1]))
         with open('data_0.txt', 'w') as file:
             pprint.pprint(haha_0, stream=file)
@@ -119,17 +125,21 @@ def writeReadDict2Txt(choice):
             haha_0 = eval(file.read())
         with open('data.txt', 'r') as file:
             haha = eval(file.read())
-        haha1 = haha_0 | haha  #Python 3.9+: The merge operator | now works for dictionaries:
+        haha1 = haha_0 | haha  # Python 3.9+: The merge operator | now works for dictionaries:
         return haha1
 
-#Define a function to clear the content of the text widget
+
+# Define a function to clear the content of the text widget
 def click_input_field(event):
-   input_field.delete(0, "end")
-   input_field.unbind('<Button-1>', clicked_input_field)
+    input_field.delete(0, "end")
+    input_field.unbind('<Button-1>', clicked_input_field)
+
 
 def click_input_field_1(event):
-   input_field_1.delete(0, "end")
-   input_field_1.unbind('<Button-1>', clicked_input_field_1)
+    input_field_1.delete(0, "end")
+    input_field_1.unbind('<Button-1>', clicked_input_field_1)
+
+
 haha = writeReadDict2Txt(0)
 
 HELP_CMD = ""
@@ -148,7 +158,7 @@ input_label = tk.Label(root, text="T9/Single (компонент)", height=1)
 input_label.pack(fill=tk.X)
 input_field = tk.Entry(root)
 input_field.insert(0, "Ключ или неисправность")
-#Bind the Entry widget with Mouse Button to clear the content
+# Bind the Entry widget with Mouse Button to clear the content
 clicked_input_field = input_field.bind('<Button-1>', click_input_field)
 input_field.pack(padx=10, pady=5, fill=tk.X)
 
@@ -157,7 +167,7 @@ input_label_1 = tk.Label(root, text="Complex (неисправность)")
 input_label_1.pack(fill=tk.X)
 input_field_1 = tk.Entry(root)
 input_field_1.insert(0, "Неисправности")
-#Bind the Entry widget with Mouse Button to clear the content
+# Bind the Entry widget with Mouse Button to clear the content
 clicked_input_field_1 = input_field_1.bind('<Button-1>', click_input_field_1)
 input_field_1.pack(padx=10, pady=5, fill=tk.X)
 
